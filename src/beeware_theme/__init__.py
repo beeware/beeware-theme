d4e2d587997beb5bba9b82ce9340cea585da3349
Kattni <kattni@kattni.com>
2025-06-26 23:05:19 -0400

    Add logo, integrate html_theme_options.

--- src/beeware_theme/__init__.py
+++ src/beeware_theme/__init__.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 
 
-def init(project_name, templates, context, static, css):
+def init(project_name, templates, context, static, css, theme_options):
     context["github_project_name"] = project_name
 
     templates.append(str(Path(__file__).parent / "_templates"))
     static.append(str(Path(__file__).parent / "_static"))
     css.append("beeware-theme.css")
+
+    theme_options.setdefault("light_css_variables", {}).update({
+        "toc-title-font-size": "var(--font-size--small--3)",  # Increase size
+        "toc-font-size": "var(--font-size--small--2)",  # Increase size
+        "sidebar-item-font-size": "var(--font-size--normal)",  # Increase size
+    })
