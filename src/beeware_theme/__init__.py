3dc71a7f07075b956957241627398a736364878e
Charles Whittington <charleswhitt@gmail.com>
2025-07-01 19:53:38 -0400

    Make init args optional

--- src/beeware_theme/__init__.py
+++ src/beeware_theme/__init__.py
@@ -1,7 +1,20 @@
 from pathlib import Path
 
 
-def init(project_name, templates, context, static, css, theme_options):
+def init(
+    project_name,
+    templates=None,
+    context=None,
+    static=None,
+    css=None,
+    theme_options=None,
+):
+    templates = [] if templates is None else templates
+    context = {} if context is None else context
+    static = [] if static is None else static
+    css = [] if css is None else css
+    theme_options = {} if theme_options is None else theme_options
+
     context["github_project_name"] = project_name
 
     templates.append(str(Path(__file__).parent / "_templates"))
